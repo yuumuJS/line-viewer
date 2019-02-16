1e7aae3d9b5db6878dbb7a04b82781610e593ecc
yuumuJS <yuumuknn@gmail.com>
2019-02-16 22:27:14 +0900

    create parser (beta)

--- parse.js
+++ parse.js
@@ -1,27 +1,42 @@
-const fs = require('fs');
+import fs from 'fs';
 
-const talk = fs.readFileSync('talk.txt', {encoding: 'utf-8'});
+const talk = fs.readFileSync('talk.txt', { encoding: 'utf-8' });
 // console.log(msg); talk.txt 取得
-const array = talk.split(/\r\n|\r|\n/);
-// console.log(array); 行ごとに配列に格納
-// console.log(array.length); 行数
+const talkArray = talk.split(/\r\n|\r|\n/);
 
-const save = /^保存日時：\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}$/;
-const empty = /^\n$/;
-const date = /^\d{4}\/\d{2}\/\d{2}\([月火水木金土日]\)$/;
-const line = /^(\d{2}:\d{2})\t(.*)\t(.*)$/;
-const paragraph = array[11].match(line);
-const time = paragraph[1];
-const user = paragraph[2];
-const text = paragraph[3];
+// const save = /^保存日時：\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}$/;
+// const empty = /^\n$/;
+const datePattern = /^(\d{4}\/\d{2}\/\d{2})\([月火水木金土日]\)$/;
+const linePattern = /^(\d{2}:\d{2})\t(.*)\t(.*)$/;
 
+const talkData = {};
 
-// console.log(array[1].match(save));
-// console.log(array[3].match(date));
-console.log(time);
-console.log(user);
-console.log(text);
+let datePoint = '';
 
+talkArray.forEach((value) => {
+    // console.log(value);
+    // 日付
+    const dateMatched = value.match(datePattern);
+    const lineMatched = value.match(linePattern);
+    if (dateMatched) {
+        // console.log('日付' + dateMatched[1]);
+        datePoint = dateMatched[1];
+        talkData[datePoint] = [];
+    }
+    // 時間から始まるトーク
+    else if (lineMatched) {
+        talkData[datePoint].push({
+            time: lineMatched[1],
+            user: lineMatched[2],
+            text: lineMatched[3]
+        });
+    }
+    // それ以外
+    else {
+        // console.log(value);
+    }
+});
 
+console.log(talkData);
 
-
+console.log(talkData['2019/01/18'][2]['text']);
