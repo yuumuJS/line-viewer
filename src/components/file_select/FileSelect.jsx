cb7cdc664ecd35235d8b7845766bd63de90842be
yuumuJS <yuumuknn@gmail.com>
2019-09-10 03:47:04 +0900

    add view filename.

--- src/components/file_select/FileSelect.jsx
+++ src/components/file_select/FileSelect.jsx
@@ -3,9 +3,23 @@ import './FileSelect.css';
 
 export default class FileSelect extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            fileName: ''
+        };
+    }
+
     handleChangeFile(e) {
         const target = e.target;
         const file = target.files.item(0);
+
+        if (!file) {
+            this.setState({fileName: ''});
+            return;
+        }
+
+        this.setState({fileName: file.name});
         const fileReader = new FileReader();
         fileReader.onload = () => {
             this.props.onSelected(file.name, fileReader.result);
@@ -18,7 +32,9 @@ export default class FileSelect extends Component {
             <label>
                 ファイルを選択
                 <input type='file' onChange={(e) => {this.handleChangeFile(e)}} />
+                {this.state.fileName ? <p className="fileName">{this.state.fileName}</p> : null}
             </label>
         );
     }
+
 }
