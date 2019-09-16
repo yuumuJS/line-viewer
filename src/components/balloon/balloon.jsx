12d131e7142e4344ba5c267b5b5aa83965ed64c8
yuumuJS <yuumuknn@gmail.com>
2019-09-16 23:31:34 +0900

    Setting prettier.

--- src/components/balloon/balloon.jsx
+++ src/components/balloon/balloon.jsx
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './balloon.css';
 
 class Balloon extends Component {
-	render() {
-		return (
-			<div className={`balloon ${this.props.myTalk ? 'right' : 'left'}`}>
-				<p className='user'>{this.props.user}</p>
-				<p className='text'>{this.props.text}</p>
-				<p className='time'>{this.props.time}</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className={`balloon ${this.props.myTalk ? 'right' : 'left'}`}>
+        <p className="user">{this.props.user}</p>
+        <p className="text">{this.props.text}</p>
+        <p className="time">{this.props.time}</p>
+      </div>
+    );
+  }
 }
 
 export default Balloon;
